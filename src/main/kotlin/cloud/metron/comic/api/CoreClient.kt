134b0d46c9f6a782ad4069ca48194a861ec8c4b2
Wizard <lenkgit@gmail.com>
2023-11-09 21:59:08 -0500

    Add: Debugging support

--- src/main/kotlin/cloud/metron/comic/api/CoreClient.kt
+++ src/main/kotlin/cloud/metron/comic/api/CoreClient.kt
@@ -1,13 +1,21 @@
 package cloud.metron.comic.api
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-open class CoreClient(credentials: Credentials, baseUrl: String) {
+
+open class CoreClient(credentials: Credentials, baseUrl: String, debugging: Boolean = false) {
     // custom [OkHttpClient] client to add an authentication interceptor
     private var httpClient: OkHttpClient = OkHttpClient.Builder().apply {
         addInterceptor(AuthenticatorInterceptor(credentials.username, credentials.password))
+
+        if (debugging) {
+            addInterceptor(HttpLoggingInterceptor().apply {
+                setLevel(HttpLoggingInterceptor.Level.BODY)
+            })
+        }
     }.build()
 
     // custom [Retrofit] client to add custom [OkHttpClient] and add Gson (JSON) support
